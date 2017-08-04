f2dd4b77ac44b5cd35964400adf572d5f6bf912d
Chris Comiskey <cwcomiskey@gmail.com>
2017-08-04 12:26:49 -0700

    Delete runExample2.R

--- R/runExample2.R
+++
@@ -1,9 +0,0 @@
-#' @export
-runExample2 <- function() {
-  appDir <- system.file("shiny-mapapp", package = "mapapp")
-  if (appDir == "") {
-    stop("Could not find example directory.
-         Try re-installing `mypackage`.", call. = FALSE)
-  }
-  shiny::runApp(appDir, display.mode = "normal")
-}
